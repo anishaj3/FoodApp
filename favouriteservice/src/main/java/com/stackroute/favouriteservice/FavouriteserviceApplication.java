3899fa2f672aaf8d7eb82cf06eaaa740bc730865
Hema Anand V <hema.v@stackroute.in>
2019-03-25 12:45:40 +0530

    Renamed favourite folder

--- favouriteservice/src/main/java/com/stackroute/favouriteservice/FavouriteserviceApplication.java
+++ favouriteservice/src/main/java/com/stackroute/favouriteservice/FavouriteserviceApplication.java
@@ -1,10 +1,10 @@
-package com.example.favoriteservice;
+package com.stackroute.favouriteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FavoriteserviceApplication {
+public class FavouriteserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FavoriteserviceApplication.class, args);
