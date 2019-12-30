1cfac3c7e3cc468a0c95f474ee630f9b0358b868
anisha <anisha.j2@cognizant.com>
2019-12-30 13:39:25 +0530

    Added Repo

---
+++ favouriteservice/src/main/java/com/stackroute/favouriteservice/repo/FavouriteRepository.java
@@ -0,0 +1,15 @@
+package com.stackroute.favouriteservice.repo;
+
+import com.stackroute.favouriteservice.domain.Favourite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @author Navya Surendran
+ *
+ */
+public interface FavouriteRepository extends JpaRepository<Favourite, Integer> {
+
+	List<Favourite> findByUserId(String userId);
+}
