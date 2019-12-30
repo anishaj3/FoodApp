8af2efbf7539a3f64fc8e2bbc08b7d3c5dcedc56
anisha <anisha.j2@cognizant.com>
2019-12-30 13:29:48 +0530

    Created domain

---
+++ favouriteservice/src/main/java/com/stackroute/favouriteservice/domain/Favourite.java
@@ -0,0 +1,70 @@
+package com.stackroute.favouriteservice.domain;
+
+import javax.persistence.*;
+
+/**
+ * @author Navya Surendran
+ *
+ */
+@Entity
+@Table(name="fooddb")
+public class Favourite {
+
+	@Id
+    @Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int ndbno;
+
+	@Column(name = "itemName")
+	private String name;
+
+	@Column(name = "user_id")
+	private String userId;
+
+	@Column(name= "foodGroup")
+	private String foodGroup;
+
+	public String getFoodGroup() {
+		return foodGroup;
+	}
+
+	public void setFoodGroup(String foodGroup) {
+		this.foodGroup = foodGroup;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNdbno() {
+		return ndbno;
+	}
+
+	public void setNdbno(Integer ndbno) {
+		this.ndbno = ndbno;
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Favourite(){}
+
+	public Favourite(String userId, String itemName, Integer id, String foodGroup) {
+		super();
+		this.name = itemName;
+		this.ndbno = id;
+		this.userId = userId;
+		this.foodGroup = foodGroup;
+	}
+
+}
