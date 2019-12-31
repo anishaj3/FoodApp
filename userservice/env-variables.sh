7a024b36b4b9b2e9ab2ab72d84d306d164e3fc60
anisha <anisha.j2@cognizant.com>
2019-12-31 09:17:08 +0530

    Updated CI

--- userservice/env-variables.sh
+++ userservice/env-variables.sh
@@ -1,4 +1,4 @@
 export MYSQL_CI_URL=jdbc:mysql://localhost:3306/fooddb?verifyServerCertificate=false&useSSL=false&requireSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC
 export MYSQL_USER=root
-export MYSQL_PASSWORD=password
+export MYSQL_PASSWORD=root
 export MYSQL_DATABASE=fooddb
